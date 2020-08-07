4e3a53aa27937a8bb0f31273711595104d18a852
Robert <robertgordonkey@gmail.com>
2020-08-07 09:31:42 +0200

    forgot to change the version of mbl.pl set to 1.32

--- usr/local/bin/mbl.pl
+++ usr/local/bin/mbl.pl
@@ -620,7 +620,7 @@ sub findbitlockerdevices {
 # main entry point
 ############################
 # the version
-my $version = "1.3";
+my $version = "1.32";
 
 # the hash contains password and mount point for bitlocker drives.
 # the key is the partuuid
