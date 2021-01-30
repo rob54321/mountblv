1fd5680df37b2d15aae7a5d68f27cea6d4fb443b
Robert <robertkey@mighty.co.za>
2021-01-30 08:55:59 +0200

    developed the constructor of DataMan, needs testing

--- usr/local/lib/site_perl/DataMan.pm
+++ usr/local/lib/site_perl/DataMan.pm
@@ -30,12 +30,62 @@ use warnings;
 # resource file
 my $rcfile = "$ENV{'HOME'}/.mbldata.rc";
 
+# class variables
+my $blref;
+my $vfref;
+
 # constructor.
 # read the file and fill the %allbldev and %vdevice hashes
 # if the file does not exist, call the method to create a default one
-# the constructor takes no parameters.
+# the constructor takes two parameters
+# DataMan(\%bitlockerdevs, \%veradevs)
 sub new {
 	my $class = shift;
+	$blref = shift;
+	$vfref = shift;
 	
-
+	# if the resource file exists, open and read it
+	# else create a default one
+	if (open DATA, "<", $rcfile) {
+		# get line
+		while (my $line = <DATA>) {
+			# check if line is a bitlocker or vera file
+			chomp($line);
+			my @record = split /:/,$line;
+			if ($record[0] eq "b") {
+				# the record is a bitlocker record
+				# %allbldev{partuuid} = [diskmountpt, disklabel]
+				# each line:
+				# for bitlocker drive:
+				# b:partuuid:mount point:partition label
+				#
+				$blref->{$record[1]} = [$record[2], $record[3]];
+			} elsif ($record[0] eq "v") {
+				# the record is a vera file
+				# %vdevice{partition_label} = [drive mountpoint, {verafile => vera_mountpoint}]
+				# each line:
+				# v:partition label:partition mount point:full verafile name:vera mountpiont
+				#
+				$vfref->{$record[1])} = [$record[2], {$record[3] => $record[4]}];
+			} else {
+				# unknown record
+				print "$line is unkown\n";
+			}
+		} # end while
+		close DATA;
+	} else {
+		# resource file does not exist
+		# create a new one.
+		$class->createdefaultdata()
+	} # end if open
+	
+	return bless {}, $class;
 }
+
+# method to create a default resource file
+# the only parameter is the ref to the class
+sub createdefaultdata {
+	my $self = shift;
+	
+# obligatory end of class
+1;
