bcebdfbb32e3aa5ded5163bdc8b917186943de39
Robert <robertkey@mighty.co.za>
2021-02-14 10:36:50 +0200

    "postinst now sets permissions of .mbl.rc and .mbldata.rc to robert.robert"

--- usr/local/bin/mbl.pl
+++ usr/local/bin/mbl.pl
@@ -12,7 +12,7 @@ my $passman;
 my $dataman;
 
 # the version
-my $version = "2.15";
+my $version = "2.16";
 
 # read fstab into array to check for disk mounts in fstab
 # only used in mountveracontainer
