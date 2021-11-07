15815209f25ae47af8efa118a4ed32c658bdbd3b
Robert <robertkey@mighty.co.za>
2021-11-07 08:44:49 +0200

    changed version in mbl.pl from 2.24 to 2.26

--- usr/local/bin/mbl.pl
+++ usr/local/bin/mbl.pl
@@ -12,7 +12,7 @@ my $passman;
 my $dataman;
 
 # the version
-my $version = "2.24";
+my $version = "2.26";
 
 # read fstab into array to check for disk mounts in fstab
 # only used in mountveracontainer
